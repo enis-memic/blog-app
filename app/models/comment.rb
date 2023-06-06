8cb5ef8d92927a783bfd8be09d6143f93a4e1554
Enis <enis.mema@gmail.com>
2023-06-06 16:06:20 +0200

    Fix linters

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,10 @@
 class Comment < ApplicationRecord
-    belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-    belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
 
-     def update_comments_counter
-        post.increment!(:comments_counter)
-    end
+  def update_comments_counter
+    post.increment!(:comments_counter)
+  end
 
-    after_save :update_comments_counter
+  after_save :update_comments_counter
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,12 +1,9 @@
 class Like < ApplicationRecord
-    belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-    belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
 
-    
-    
-    
-    def update_likes_counter
-        post.increment!(:likes_counter)
-    end
-    after_save :update_likes_counter
+  def update_likes_counter
+    post.increment!(:likes_counter)
+  end
+  after_save :update_likes_counter
 end
