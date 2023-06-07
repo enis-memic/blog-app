0c054f9ccaade793a38679ebea21c5ad02c5d9b0
Enis <enis.mema@gmail.com>
2023-06-07 12:17:09 +0200

    Validation for User

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,8 @@ class Post < ApplicationRecord
   has_many :likes, foreign_key: 'post_id'
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
+   
+
   def recent_comments
     comments.order(created_at: :desc).limit(5)
   end
