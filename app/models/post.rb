9867075dcd8ca3cd8875ef65f6741d3f63ed9eda
Enis <enis.mema@gmail.com>
2023-06-07 12:23:35 +0200

    Validation for Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,9 @@ class Post < ApplicationRecord
   has_many :likes, foreign_key: 'post_id'
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
-   
+   validates :title, presence:true, length: {maximum: 250}
+   validates :comments_counter, numericality: {only_integer: true}, comparison: {greater_than_or_equal_to: 0}
+   validates :likes_counter, numericality: {only_integer: true}, comparison: {greater_than_or_equal_to: 0}
 
   def recent_comments
     comments.order(created_at: :desc).limit(5)
