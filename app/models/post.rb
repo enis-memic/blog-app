d7c427d9f00cbd0d5b7e450106943be65be6b946
Enis <enis.mema@gmail.com>
2023-06-07 12:56:20 +0200

    Add test for comments and method

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,7 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
    validates :title, presence:true, length: {maximum: 250}
-   validates :comments_counter, numericality: {only_integer: true}, comparison: {greater_than_or_equal_to: 0}
+   validates :comment_counter, numericality: {only_integer: true}, comparison: {greater_than_or_equal_to: 0}
    validates :likes_counter, numericality: {only_integer: true}, comparison: {greater_than_or_equal_to: 0}
 
   def recent_comments
@@ -12,7 +12,7 @@ class Post < ApplicationRecord
   end
 
   def update_posts_counter
-    author.increment!(:posts_counter)
+    author.increment!(:post_counter)
   end
   after_save :update_posts_counter
 end
