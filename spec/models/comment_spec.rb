3b1662a6f478ceff3822045bfab92ec2b3a4df9b
Enis <enis.mema@gmail.com>
2023-06-07 14:52:24 +0200

    Fix linters

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,15 +1,16 @@
 require 'rails_helper'
 
-describe Comment, type: :model do 
-    before :each do
-        @author = User.new(name: 'Enis', photo: 'https://avatars.githubusercontent.com/u/118063058?v=4', bio: 'Full-stack developer')
-        @post = Post.new(author: @author, title: 'Post one', text: 'Post')
+describe Comment, type: :model do
+  before :each do
+    @author = User.new(name: 'Enis', photo: 'https://avatars.githubusercontent.com/u/118063058?v=4',
+                       bio: 'Full-stack developer')
+    @post = Post.new(author: @author, title: 'Post one', text: 'Post')
 
-        @comment1 = Comment.create(author: @author, post: @post, text: 'Comment one')
-        @comment2 = Comment.create(author: @author, post: @post, text: 'Comment two')
-      end
+    @comment1 = Comment.create(author: @author, post: @post, text: 'Comment one')
+    @comment2 = Comment.create(author: @author, post: @post, text: 'Comment two')
+  end
 
-      it 'will have the Posts comments_counter through update_comments_counter ' do
-        expect(@post.comment_counter).to eq 2
-      end
+  it 'will have the Posts comments_counter through update_comments_counter ' do
+    expect(@post.comment_counter).to eq 2
+  end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,38 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-    subject {Post.new(title: 'My Post', comment_counter: 10, likes_counter: 10)}
+  subject { Post.new(title: 'My Post', comment_counter: 10, likes_counter: 10) }
 
-    before {subject.save}
+  before { subject.save }
 
-    it 'Title should be present' do
+  it 'Title should be present' do
     subject.title = nil
     expect(subject).to_not be_valid
-    end
+  end
 
   it 'Title length should be maximum 250' do
-  subject.title = 'a' * 300
-  expect(subject).to_not be_valid
+    subject.title = 'a' * 300
+    expect(subject).to_not be_valid
   end
 
   it 'Comment counter should be integer' do
     subject.comment_counter = 'z'
     expect(subject).to_not be_valid
-    end
+  end
 
-   it 'Comment counter should be 0 or greater' do
+  it 'Comment counter should be 0 or greater' do
     subject.comment_counter = -1
     expect(subject).to_not be_valid
-   end 
+  end
 
-   it 'Likes counter should be integer' do
+  it 'Likes counter should be integer' do
     subject.likes_counter = 'z'
     expect(subject).to_not be_valid
-    end
+  end
 
-   it 'Likes counter should be 0 or greater' do
+  it 'Likes counter should be 0 or greater' do
     subject.likes_counter = -1
     expect(subject).to_not be_valid
-   end 
-
+  end
 end
