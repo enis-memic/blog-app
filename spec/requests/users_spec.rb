65f67a7ece7a34c007759daa4aa455614b14b416
Enis <enis.mema@gmail.com>
2023-06-12 14:59:32 +0200

    Add test for users

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -3,15 +3,17 @@ require 'rails_helper'
 RSpec.describe 'Users', type: :request do
   describe 'GET /index' do
     it 'returns http success' do
-      get '/users/index'
+      get '/'
       expect(response).to have_http_status(:success)
+      expect(response.body).to include('List of all users')
     end
   end
 
   describe 'GET /show' do
     it 'returns http success' do
-      get '/users/show'
+      get '/users/:id'
       expect(response).to have_http_status(:success)
+      expect(response.body).to include('List of all users by id')
     end
   end
 end
