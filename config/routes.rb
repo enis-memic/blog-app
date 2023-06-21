7c9b11e6cd7b23954ce99fe2acf3a34f5212c6bc
shubham <shubham.bhoyar077@gmail.com>
2023-06-21 18:31:30 +0530

    Add delete route for commrnt

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:show] do
     resources :posts, only: [:new, :create, :index, :show, :destroy] do
-      resources :comments, only: [:new, :create]
+      resources :comments, only: [:new, :create, :destroy]
       resources :likes, only: [:create]
     end
   end
