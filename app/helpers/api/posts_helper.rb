a332000caa97a33c93894ace4e548b657ca384ff
Enis <enis.mema@gmail.com>
2023-06-22 14:37:05 +0200

    Fix linters

--- app/helpers/api/posts_helper.rb
+++ app/helpers/api/posts_helper.rb
@@ -1,3 +1,2 @@
 module Api::PostsHelper
-    
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,  :jwt_authenticatable, jwt_revocation_strategy: self
+         :recoverable, :rememberable, :validatable, :jwt_authenticatable, jwt_revocation_strategy: self
   has_many :likes, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
   has_many :posts, foreign_key: 'author_id'
