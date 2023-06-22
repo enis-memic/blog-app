3e92fc5c27f4c16fb303d155bf1e234e6d850df4
shubham <shubham.bhoyar077@gmail.com>
2023-06-22 18:03:30 +0530

    Add controller for comment get and post method

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,28 +1,28 @@
-# class Api::CommentsController < ApplicationController
-#     skip_before_action :verify_authenticity_token
-#  def index
-#           post = Post.find(params[:post_id])
-#           comments = post.comments.includes(:author)
-#           render json: comments
-#         end
+class Api::CommentsController < ApplicationController
+    skip_before_action :verify_authenticity_token
+ def index
+          post = Post.find(params[:post_id])
+          comments = post.comments.includes(:author)
+          render json: comments
+        end
       
-#         def create
-#           current_post = Post.find(params[:post_id])
-#           comments = current_post.comments.new(comment_params)
-#           comments.post_id = current_post.id
-#           comments.author_id = current_user.id
+        def create
+          current_post = Post.find(params[:post_id])
+          comments = current_post.comments.new(comment_params)
+          comments.post_id = current_post.id
+          comments.author_id = current_user.id
       
-#           if comments.save
-#             render json: comments, status: :created
-#           else
-#             render json: comments.errors, status: :unprocessable_entity
-#           end
-#         end
+          if comments.save
+            render json: comments, status: :created
+          else
+            render json: comments.errors, status: :unprocessable_entity
+          end
+        end
       
-#         private
+        private
       
-#         def comment_params
-#           params.require(:comment).permit(:text)
-#         end
+        def comment_params
+          params.require(:comment).permit(:text)
+        end
     
-# end
+end
